ca6dd7b4566ab1d6596c90a77c0be3ede9f9fa98
Adammui <adami.yuje@gmail.com>
2021-10-20 19:50:49 +0300

    a

--- script.rb
+++ script.rb
@@ -8,25 +8,27 @@ require 'csv'
 
 puts 'Page address:'
 # gets
-puts url = 'https://www.petsonic.com/farmacia-para-gatos/'
+puts url = 'https://www.petsonic.com/gatos/'
 puts 'Put file name:'
 #filename = gets
 
-#html = URI.open(url)
-html = Curl.get(url)
-doc = Nokogiri::HTML(html.body_str)
-#html = Curl.get(url)
-#puts html.body_str
-
+html = URI.open(url)
 doc = Nokogiri::HTML(html)
+#html = Curl.get(url)
+#doc = Nokogiri::HTML(html.body_str)
 
-rows = doc.xpath('//*[@id="st_advanced_ma_9046"]')
-details = rows.collect do |row|
-  detail = {}
-  [
-    [:title, '@title']
-  ].each do |name, xpath|
-    puts row.at_xpath(xpath).to_s.strip
+products = doc.xpath('//*[@id="product_list"]')
+details = products.collect do |product|
+  (0..25).each do |i|
+    detail = {}
+    [
+      [:pic, "li[#{i}]/div[1]/div[2]/div[2]/a/img/@src"],
+      [:price, "li[#{i}]/div[1]/div[2]/div[3]/div[2]/div[1]/span/text()"],
+      [:cents, "li[#{i}]/div[1]/div[2]/div[3]/div[2]/div[1]/span/span/text()"],
+      [:title, "li[#{i}]/div[1]/div[2]/div[3]/div[1]/div/a/@title"]
+    ].each do |name, xpath|
+      puts product.at_xpath(xpath).to_s.strip
+    end
+    detail
   end
-  detail
 end
