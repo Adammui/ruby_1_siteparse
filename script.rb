c8136bc8bf92de55a0d2dff31cf5a9373ab983a9
Adammui <adami.yuje@gmail.com>
2021-10-21 19:43:42 +0300

    a

--- script.rb
+++ script.rb
@@ -6,29 +6,44 @@ require 'curb'
 require 'csv'
 
 
-puts 'Page address:'
+print 'Page address:'
 # gets
-puts url = 'https://www.petsonic.com/gatos/'
-puts 'Put file name:'
-#filename = gets
+puts url = 'https://www.petsonic.com/cicatrizantes-para-gatos/'
+print 'Put file name:'
+puts filename = gets
 
 html = URI.open(url)
 doc = Nokogiri::HTML(html)
 #html = Curl.get(url)
 #doc = Nokogiri::HTML(html.body_str)
 
-products = doc.xpath('//*[@id="product_list"]')
-details = products.collect do |product|
-  (0..25).each do |i|
-    detail = {}
-    [
-      [:pic, "li[#{i}]/div[1]/div[2]/div[2]/a/img/@src"],
-      [:price, "li[#{i}]/div[1]/div[2]/div[3]/div[2]/div[1]/span/text()"],
-      [:cents, "li[#{i}]/div[1]/div[2]/div[3]/div[2]/div[1]/span/span/text()"],
-      [:title, "li[#{i}]/div[1]/div[2]/div[3]/div[1]/div/a/@title"]
-    ].each do |name, xpath|
-      puts product.at_xpath(xpath).to_s.strip
+print 'Found product count: '
+puts product_count = doc.xpath('//*[@id="nb_item_bottom"]/@value').to_s.strip.to_i
+print 'Page count (25 products): '
+puts page_count = (product_count / 25.0).ceil
+CSV.open(filename, 'wb') do |csv_line|
+  csv_line << ['Product name', 'Product price', 'Product image']
+  (0..page_count).each do |i|
+    if i > 1.5
+      link = url + "?p=#{i}"
+      html = URI.open(link)
+      doc = Nokogiri::HTML(html)
     end
-    detail
+    products = doc.xpath('//*[@id="product_list"]')
+    products.collect do |product|
+      (0..25).each do |i|
+        title = product.at_xpath( "li[#{i}]/div[1]/div[2]/div[3]/div[1]/div/a/@title").to_s.strip
+        price = product.at_xpath( "li[#{i}]/div[1]/div[2]/div[3]/div[2]/div[1]/span/text()").to_s.strip
+        + "li[#{i}]/div[1]/div[2]/div[3]/div[2]/div[1]/span/span/text()".to_s.strip
+        pic = product.at_xpath("li[#{i}]/div[1]/div[2]/div[2]/a/img/@src").to_s.strip
+          #print "#{product.at_xpath(xpath).to_s.strip} "
+        line = ["#{title} - #{"ves"}", price, pic ]
+        csv_line << line
+      end
+      puts
+    end
   end
 end
+
+puts 'File Write is successful'
+
