157c612852f419687af38dab04b1e351456f216a
Adammui <adami.yuje@gmail.com>
2021-10-22 12:41:09 +0300

    a

--- script.rb
+++ script.rb
@@ -5,9 +5,7 @@ require 'nokogiri'
 require 'curb'
 require 'csv'
 
-
 print 'Page address:'
-# gets
 puts url = 'https://www.petsonic.com/cicatrizantes-para-gatos/'
 print 'Put file name:'
 puts filename = gets
@@ -25,6 +23,7 @@ print 'Found product count: '
 puts product_count = doc.xpath('//*[@id="nb_item_bottom"]/@value').to_s.strip.to_i
 print 'Page count (25 products): '
 puts page_count = (product_count / 25.0).ceil
+
 CSV.open(filename, 'wb') do |csv_line|
   csv_line << ['Product name', 'Product price', 'Product image']
   puts 'Opened csv file, going to run loop'
@@ -41,17 +40,10 @@ CSV.open(filename, 'wb') do |csv_line|
     products = doc.xpath('//*[@id="product_list"]')
     products.collect do |product|
       (0..25).each do |i|
-        #productdoc=product.at_xpath("li[#{i}]/div[1]/div[2]/div[1]/a/@href").to_s.strip
-        #puts productdoc
-        #html1 = URI.open(productdoc)
-        #doc1 = Nokogiri::HTML(html1)
-        #title = doc1.at_xpath('/html/body/div[1]/div[1]/div[5]/div[1]/div/div/div/div[2]/div[2]/div[1]/div/h1/text()').to_s.strip
-
         title = product.at_xpath("li[#{i}]/div[1]/div[2]/div[3]/div[1]/div/a/@title").to_s.strip
         price = product.at_xpath("li[#{i}]/div[1]/div[2]/div[3]/div[2]/div[1]/span/text()").to_s.strip
         cents= product.at_xpath("li[#{i}]/div[1]/div[2]/div[3]/div[2]/div[1]/span/span/text()").to_s.strip
         pic = product.at_xpath("li[#{i}]/div[1]/div[2]/div[2]/a/img/@src").to_s.strip
-        #print "#{product.at_xpath(xpath).to_s.strip} "
         line = [title.to_s, price.to_s + cents.to_s, pic]
         next if title.to_s.empty?
         csv_line << line
